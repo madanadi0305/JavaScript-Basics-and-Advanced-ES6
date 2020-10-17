df98457f9c3423bdd4138e0763af1d73c345ee10
madanadi0305 <madanadi0305@gmail.com>
2020-10-17 05:46:03 +0000

    Update person.js, task05.js files

--- person.js
+++ person.js
@@ -20,5 +20,5 @@ return this.Name.firstName+" "+this.Name.lastName+" is "+this.Age+" years old.Th
    return "Hi! I'm "+this.Name.firstName+" "+this.Name.lastName;
  }
 }
-module.exports=Person;
+module.exports={class:Person};
 
--- task05.js
+++ task05.js
@@ -1,5 +1,5 @@
-import Person from './person.js';
+var person=require('./person.js');
 var person1=new Person(['John','Snow'],30,'M','Music');
-
+person1.
 
 module.exports=person1;
