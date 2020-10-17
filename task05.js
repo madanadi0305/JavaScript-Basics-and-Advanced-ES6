ed5f3ecf202cb3b058a0cc8bf4d710fc5c8c593b
madanadi0305 <madanadi0305@gmail.com>
2020-10-17 06:43:48 +0000

    Update task05.js

--- task05.js
+++ task05.js
@@ -1,5 +1,5 @@
-import {Person} from './person.js';
-var person1=new Person(['John','Snow'],30,'M','Music');
+var person=require('./person.js');
+var person1= person(['John','Snow'],30,'M','Music');
 
 
 module.exports=person1;
