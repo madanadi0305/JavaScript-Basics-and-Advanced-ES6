898acc825ace42e26f0446d737c718705572b788
madanadi0305 <madanadi0305@gmail.com>
2020-10-17 07:32:56 +0000

    Update wiki/task06.md, person.js files

---
+++ person.js
@@ -0,0 +1,24 @@
+class Person{
+  Name=['firstName','lastName'];
+  Age;
+  Gender;
+  Interests;
+
+constructor(name,age,gender,interests){
+this.Name=name;
+this.Age=age;
+this.Gender=gender;
+this.Interests=interests;
+
+}
+
+ Bio(){
+return this.Name[0]+" "+this.Name[1]+" is "+this.Age+" years old. They like "+this.Interests+".";
+
+ }
+ Greeting(){
+   return "Hi! I'm "+this.Name[0]+" "+this.Name[1]+".";
+ }
+}
+module.exports=Person;
+
