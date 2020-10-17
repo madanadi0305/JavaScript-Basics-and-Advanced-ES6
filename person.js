7fec938c244d1e224ee7dfd5071ef03a791a7fc5
madanadi0305 <madanadi0305@gmail.com>
2020-10-17 06:55:48 +0000

    Update person.js

--- person.js
+++ person.js
@@ -13,11 +13,11 @@ this.Interests=interests;
 }
 
  Bio(){
-return this.Name[0]+" "+this.Name[1]+" is "+this.Age+" years old.They like "+this.Interests;
+return this.Name[0]+" "+this.Name[1]+" is "+this.Age+" years old. They like "+this.Interests;
 
  }
  Greeting(){
-   return "Hi! I'm "+this.Name.firstName+" "+this.Name.lastName;
+   return "Hi! I'm "+this.Name[0]+" "+this.Name[1];
  }
 }
 module.exports=Person;
