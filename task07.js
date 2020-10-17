4cffae720e5159039ee4d5ff599028143a06cc05
madanadi0305 <madanadi0305@gmail.com>
2020-10-17 16:52:13 +0000

    Update task07.js

--- task07.js
+++ task07.js
@@ -6,7 +6,7 @@ for(var i=1;(i*i)<n;i++)
     perfect_square_sum=perfect_square_sum+i*i;
 }
 var map1=new Map();
-map1.set(perfect_square_sum,perfect_square_sum);
+map1.set('perfect_square_sum',perfect_square_sum);
 return map1;
 }
 module.exports=task07;
