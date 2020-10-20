0bac3fe4cfff1295c34c6517afb9980b8c1680d5
madanadi0305 <madanadi0305@gmail.com>
2020-10-20 05:30:14 +0000

    Update task06.js, person.js files

--- person.js
+++ person.js
@@ -1,14 +1,12 @@
 class Person{
- Name;
+ Name=[];
   Age;
   Gender;
   Interests;
 
 constructor(firstName,lastName,age,gender,interests){
-this.Name={
-  firstName,
-  lastName
-};
+this.Name.push(firstName);
+this.Name.push(lastName);
 this.Age=age;
 this.Gender=gender;
 this.Interests=interests;
@@ -16,11 +14,11 @@ this.Interests=interests;
 }
 
  Bio(){
-return this.firstName+" "+this.lastName+" is "+this.Age+" years old. They like "+this.Interests+".";
+return this.Name[0]+" "+this.Name[1]+" is "+this.Age+" years old. They like "+this.Interests+".";
 
  }
  Greeting(){
-   return "Hi! I'm "+this.firstName+" "+this.lastName+".";
+   return "Hi! I'm "+this.Name[0]+" "+this.Name[1]+".";
  }
 
 
--- task06.js
+++ task06.js
@@ -1,12 +1,16 @@
 var Person=require('./person.js');
 class Teacher extends Person{
+Name=[];    
 constructor(firstName,lastName,age,gender,interests,subject){
 super(firstName,lastName,age,gender,interests);
 this.Subject=subject;
-this.firstName=firstName;
-this.lastName=lastName;
-this.age=age;
-this.interests=interests;
+//this.firstName=
+this.Name.push(firstName);
+//this.lastName=
+this.Name.push(lastName);
+this.Age=age;
+this.Gender=gender;
+this.Interests=interests;
 }
 Greeting(){
 var salutation;
@@ -17,30 +21,32 @@ if(this.Gender==='M'){
 else{
     salutation='Mrs. ';
 }
-return "Hello. My name is "+salutation+this.lastName+", and I teach "+this.Subject+".";
+return "Hello, My name is "+salutation+this.Name[1]+", and I teach "+this.Subject+".";
 }
 Bio(){
-   return this.firstName+" "+this.lastName+" is "+this.Age+" years old. They like "+this.Interests+"."; 
+   return this.Name[0]+" "+this.lastName[1]+" is "+this.Age+" years old. They like "+this.Interests+"."; 
 }
 
 
 }
 class Student extends Person{
-
+Name=[];
 constructor(firstName,lastName,age,gender,interests){
 super(firstName,lastName,age,gender,interests);
-this.firstName=firstName;
-this.lastName=lastName;
+//this.firstName=
+this.Name.push(firstName);
+//this.lastName=
+this.Name.push(lastName);
 this.Age=age;
 this.Gender=gender;
 this.Interests=interests;
 }
 Bio(){
-  return  this.firstName+" "+this.lastName+" is "+this.Age+" years old. They like "+this.Interests;
+  return  this.Name[0]+" "+this.Name[1]+" is "+this.Age+" years old. They like "+this.Interests;
 }
 
 Greeting(){
-return "Yo! I'm "+this.firstName;
+return "Yo! I'm "+this.Name[0];
 
 }
 }
